e46fdea2d1a806ae8069d47152986945ba359983
Wenjie Mo <jackymo@usc.edu>
2022-07-01 12:43:20 -0400

    4

--- Hackervoice/index.js
+++ Hackervoice/index.js
@@ -6,7 +6,7 @@ module.exports = async function (context, req) {
 
 
     let result =""; 
-    //param name is password why
+    //param name is password why 
     var password = req.query.password; 
     context.log(password)
 
