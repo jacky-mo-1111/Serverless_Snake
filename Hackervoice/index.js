9a91177d64e4e9210a46af17c4869c0a543be79e
Wenjie Mo <jackymo@usc.edu>
2022-07-01 00:22:32 -0400

    let me in

--- Hackervoice/index.js
+++ Hackervoice/index.js
@@ -1,14 +1,24 @@
+const { result } = require("cypress/types/lodash");
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+
+    let result ="" 
     //param name is password
     var password = req.query.password; 
     context.log(password)
     //xiaoxie
 
+    if (password == "letmein"){
+        result = "Access granted."
+    }
+    else{
+        result = "Access denied."
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: result
     };
 }
