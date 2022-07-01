7b4a095fc70c03f7ea60472d93ff164494bfdc2b
Wenjie Mo <jackymo@usc.edu>
2022-07-01 01:41:56 -0400

    unfinished step 4

--- Hackervoice/index.js
+++ Hackervoice/index.js
@@ -4,11 +4,11 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
 
-    let result ="" 
+    let result =""; 
     //param name is password
     var password = req.query.password; 
     context.log(password)
-    //xiaoxie
+
 
     if (password == "letmein"){
         result = "Access granted."
