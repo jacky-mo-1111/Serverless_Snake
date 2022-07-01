5ad27d7e7c18a33a128f989da3d4efffacfc28f7
Wenjie Mo <jackymo@usc.edu>
2022-07-01 00:04:20 -0400

    xiaoxie

--- Hackervoice/index.js
+++ Hackervoice/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     //param name is password
     var password = req.query.password; 
     context.log(password)
-    //
+    //xiaoxie
 
 
     context.res = {
